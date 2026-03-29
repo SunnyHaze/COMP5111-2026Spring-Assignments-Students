33bded3e5ae6220fdbca7d28b35251cfba3024f1
S. CHEN <i9s.chen@connect.ust.hk>
2026-03-29 20:44:15 +0800

    Release programming assignment 2.

---
+++ scripts/install_latest_evosuite.sh
@@ -0,0 +1,7 @@
+mvn install:install-file \
+   -Dfile=./lib/evosuite-1.2.0.jar \
+   -DgroupId=org.evosuite \
+   -DartifactId=evosuite \
+   -Dversion=1.2.0 \
+   -Dpackaging=jar \
+   -DgeneratePom=true
